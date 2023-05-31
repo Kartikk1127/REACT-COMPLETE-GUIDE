30a2228e6226027365b018b207f412557abfa059
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-31 08:42:33 +0530

    props sharing

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { createElement } from "react";
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  //this is how it would have been written if it was only javascript.
+
+  // const para = createElement('p');
+  // para.textContent = 'This is also visible!';
+  // document.getElementById('root').append(para);
+
+  //the below code is a jsx code which basically means that we are running html code inside a js file.
+  return (
+    <div>
+      <h2>Let's get started.</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
